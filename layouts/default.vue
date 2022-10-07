88d87fffb145cea45a296ad6451a8afd623ff1f1
데이터지능화팀/장우재 <woojae.jang@hyundai-autoever.com>
2022-10-07 17:47:47 +0900

    update section 1

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <header>
+            <h1>{{ $route.name }} 페이지</h1>
+            <nuxt-link to="/">홈페이지</nuxt-link>     
+            <nuxt-link to="/main">메인 페이지</nuxt-link>     
+            <nuxt-link to="/product">상품 페이지</nuxt-link>     
+        </header>
+        <Nuxt />
+    </div>
+  
+</template>
+
+<script>
+export default {
+    // created() {
+    //     console.log(this.$route);
+    // }
+}
+</script>
+
+<style>
+
+</style>
