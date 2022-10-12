02f178ba3fd58430e2cf7b889db4e28cd746737b
데이터지능화팀/장우재 <woojae.jang@hyundai-autoever.com>
2022-10-12 14:44:25 +0900

    update section 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,5 +39,16 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  // server setup
+  server: {
+    port: 5000,
+  },
+
+  devServer: {
+    hot: true
   }
+
 }
+
