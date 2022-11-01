3794fd49d2c004d3e429431133d9a9870a7ccd52
데이터지능화팀/장우재 <woojae.jang@hyundai-autoever.com>
2022-11-01 11:00:42 +0900

    update section 6

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -11,7 +11,7 @@
       <div class="side-panel">
         <p class="name">{{ product.name }}</p>
         <p class="price">{{ product.price }}</p>
-        <button type="button" @click="addToCart">Add to Cart</button>
+        <button type="button" @click="addToCart">카트에 담기</button>
         <!-- <NuxtLink></NuxtLink> -->
       </div>
     </div>
@@ -20,7 +20,7 @@
 
 <script>
 // import axios from 'axios';
-import { fetchProductById } from '@/api/index'
+import { createCartItem, fetchProductById } from '@/api/index'
 
 export default {
     async asyncData({ params }) {
@@ -32,7 +32,9 @@ export default {
     },
 
     methods: {
-      addToCart() {
+      async addToCart() {
+        const response = await createCartItem(this.product);
+        console.log(response);
         this.$store.commit('addCartItem', this.product);
         this.$router.push('/cart');
       }
--- store/index.js
+++ store/index.js
@@ -1,3 +1,9 @@
+import { fetchCartItems } from "~/api"
+
+// constants
+export const FETCH_CART_ITEMS = 'FETCH_CART_ITEMS'
+
+// store
 export const state = () => ({
     cartItems: [],
 })
@@ -10,4 +16,31 @@ export const mutations = {
         }
         state.cartItems.push(newCartItem)
     },
+    setCartItems(state, cartItems) {
+        state.cartItems = cartItems
+    } 
+}
+
+export const actions = {
+    async [FETCH_CART_ITEMS]({ commit }) {
+        const { data } = await fetchCartItems();
+        commit(
+            'setCartItems', data.map(item => ({
+                ...item,
+                imageUrl: `${item.imageUrl}?random=${Math.random()}`
+            }))
+        )
+        // return response
+    },
+
+    // async nuxtServerInit(storeContext, nuxtContext) {
+    //     await storeContext.dispatch(FETCH_CART_ITEMS)
+    //     // const { data } = await fetchCartItems();
+    //     // storeContext.commit(
+    //     //     'setCartItems', data.map(item => ({
+    //     //         ...item,
+    //     //         imageUrl: `${item.imageUrl}?random=${Math.random()}`
+    //     //     }))
+    //     // )
+    // }
 }
