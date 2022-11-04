2196ff99da8ef59c3efd473d42bcfd8249e73d31
데이터지능화팀/장우재 <woojae.jang@hyundai-autoever.com>
2022-11-04 18:14:58 +0900

    Revert "메타 태그 변경"
    This reverts commit 31ed8c41f46d25f009aa056a6a67d5f7f7370de9.

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,14 +3,14 @@ export default {
   
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Nuxt Shopping',
+    title: 'learn-nuxt',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt Shopping Example' },
+      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
